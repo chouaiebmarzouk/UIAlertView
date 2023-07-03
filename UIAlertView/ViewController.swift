092a742bfd892d514a6ef6054f740b72e1c55938
chouaiebmarzouk <chouaieb.marzouk1@gmail.com>
2023-07-03 15:05:24 +0100

    Initial Commit

---
+++ UIAlertView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIAlertView
+//
+//  Created by Mac air TRIMAKUS on 3/7/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
